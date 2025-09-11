fd257195103a6798d40de1123251d9a871ef34b2
JONATHAN-DEV-01 <jonathanads2006@gmail.com>
2025-09-11 19:34:40 -0300

    adição

--- config.py
+++ config.py
@@ -1,14 +1,11 @@
-from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-mysql = MySQL()
+db = SQLAlchemy()
 
 def init_app(app):
-    # Configurações do MySQL
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'         
-    app.config['MYSQL_PASSWORD'] = 'RootSenha@123'
-    app.config['MYSQL_DB'] = 'controle_gastos'
-    app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+    # Formato da URI: 'mysql+pymysql://<user>:<password>@<host>/<database>'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'mysql+pymysql://root:cavadinha@mysql/controle_gastos'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Inicializar o mysql
-    mysql.init_app(app)
+    db.init_app(app)
