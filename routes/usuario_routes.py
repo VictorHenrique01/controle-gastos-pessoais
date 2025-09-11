5d1914e484a920ac6e3f157a815ed824063ba5be
JONATHAN-DEV-01 <jonathanads2006@gmail.com>
2025-09-11 19:29:35 -0300

    adição de rotas

--- routes/usuario_routes.py
+++ routes/usuario_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify, render_template
-from models.usuario_model import cadastrar_usuario
+from models.usuario_model import cadastrar_usuario,obter_usuarios
 
 usuario_bp = Blueprint("usuario", __name__)
 
@@ -8,6 +8,11 @@ def criar_usuario():
     dados = request.get_json()
     resultado = cadastrar_usuario(dados)
     return jsonify(resultado)
+
+@usuario_bp.route("/", methods=["GET"])
+def listar_usuarios():
+    usuarios = obter_usuarios()
+    return jsonify(usuarios)
 
 @usuario_bp.route("/cadastro", methods=["GET"])
 def pagina_cadastro():
